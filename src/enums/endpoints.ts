fac32cb27b2b4e9c28266e53dc6802d8bcb36b54
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-26 00:11:15 +0100

    Add endpoints enum

---
+++ src/enums/endpoints.ts
@@ -0,0 +1,6 @@
+export enum Endpoints {
+    API_ENDPOINT = "https://discord.com/api/v10",
+    AUTHORIZE_ENDPOINT = "https://discord.com/api/oauth2/authorize",
+    TOKEN_ENDPOINT = "https://discord.com/api/oauth2/token",
+    REVOKE_ENDPOINT = "https://discord.com/api/oauth2/token/revoke",
+}

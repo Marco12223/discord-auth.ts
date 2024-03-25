9c4158e89907eb712620808be6a33a6e6169b17d
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-26 00:15:20 +0100

    Creating refreshToken method

--- src/handlers/AccessHandler.ts
+++ src/handlers/AccessHandler.ts
@@ -1,6 +1,7 @@
 import {DiscordAuth} from "../discordAuth";
 import {Scopes} from "../enums/scopes";
 import {AccessToken} from "../interfaces/user/accessToken";
+import {Endpoints} from "../enums/endpoints";
 
 export class AccessHandler {
 
@@ -17,7 +18,7 @@ export class AccessHandler {
     }
 
     public async tokenExchange(code: string): Promise<AccessToken|{message: string, code: number}> {
-        let response = await fetch("https://discord.com/api/v10/oauth2/token", {
+        let response = await fetch(Endpoints.TOKEN_ENDPOINT, {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -34,10 +35,20 @@ export class AccessHandler {
         return await response.json();
     }
 
-    public async refreshToken(refreshToken: string): Promise<any> {
-
-
-
+    public async refreshToken(refreshToken: string): Promise<AccessToken|{message: string, code: number}> {
+        let response = await fetch(Endpoints.TOKEN_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams({
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
+                grant_type: "refresh_token",
+                refresh_token: refreshToken,
+            })
+        })
+        return await response.json();
     }
 
 }
